5336d3fbbe184b078b690474b470e1bdce5a001f
Adrian Gonzalez <trabajoadrian247@gmail.com>
2024-10-10 04:29:38 +0200

    Resuelto el triangulo de gradientes con solo un VBO entrelazado

--- Renderer.cpp
+++ Renderer.cpp
@@ -246,14 +246,16 @@ namespace PAG
      */
     void Renderer::creaModelo() {
         //Los vertices con sus posiciones (x,y,z) en un VBO no entrelazado (solo posicion)
-        GLfloat posicionVertices[] = { -0.5, -0.5, 0.0,
-                               0.5, -0.5, 0.0,
-                               0.0, 0.5, 0.0};
+        GLfloat posicionVerticeColor[] = { -0.5, -0.5, 0.0,1.0, 0.0, 0.0,
+                               0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+                               0.0, 0.5, 0.0, 0.0, 0.0, 1.0};
 
+        /*
         //Otro vbo pero para el color de cada vertice
         GLfloat colorVertices[] = { 1.0, 0.0, 0.0,
                                     0.0, 1.0, 0.0,
                                     0.0, 0.0, 1.0};
+        */
 
         GLuint indices[] = {0, 1, 2}; //Los datos del IBO, que dice en que orden se procesan los vertices del VBO1
 
@@ -262,29 +264,32 @@ namespace PAG
 
         //Generamos los VBOs y IBOs
         glGenBuffers ( 1, &idVBO1);
-        glGenBuffers(1, &idVBO2);
+        //glGenBuffers(1, &idVBO2);
         glGenBuffers (1, &idIBO); //Creamos el IBO
 
         //VBO para posiciones
         glBindBuffer (GL_ARRAY_BUFFER, idVBO1);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(posicionVertices), posicionVertices, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(posicionVerticeColor), posicionVerticeColor, GL_STATIC_DRAW);
 
         //VBO para colores
-        glBindBuffer(GL_ARRAY_BUFFER, idVBO2);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(colorVertices), colorVertices, GL_STATIC_DRAW);
+        //glBindBuffer(GL_ARRAY_BUFFER, idVBO2);
+        //glBufferData(GL_ARRAY_BUFFER, sizeof(colorVertices), colorVertices, GL_STATIC_DRAW);
 
         //IBO
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idIBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-        //Configuramos ambos VBOs para que el layout del vertex shader funcione bien:
         glBindBuffer (GL_ARRAY_BUFFER, idVBO1);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), nullptr);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), nullptr);
         glEnableVertexAttribArray(0); //Habilitamos el layout 0
 
-        glBindBuffer(GL_ARRAY_BUFFER, idVBO2);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), nullptr);
-        glEnableVertexAttribArray(1); //Habilitamos el layout 1
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(sizeof(GLfloat)*3));
+        glEnableVertexAttribArray(1); //Habilitamos el layout 0
+
+        //glBindBuffer(GL_ARRAY_BUFFER, idVBO2);
+        ///Primero decimos que es para el layout 1, y luego lo habilitamos
+        //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), nullptr);
+        //glEnableVertexAttribArray(1); //Habilitamos el layout 1
 
         glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, idIBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3*sizeof(GLuint), indices, GL_STATIC_DRAW); //Le pasamos los datos al IBO
