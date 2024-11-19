883fc86dac77020e386cf1ca0038c22ee2a0dff0
Adrian Gonzalez <trabajoadrian247@gmail.com>
2024-11-19 13:39:01 +0100

    Agrego la clase Modelo que se comunicara directamente con Shader Program, que contiene la Malla
    Tambien agrego el directorio con unos guays archivos .obj

---
+++ Modelo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by secre on 19/11/2024.
+//
+
+#include "Modelo.h"
---
+++ Modelo.h
@@ -0,0 +1,23 @@
+//
+// Created by secre on 19/11/2024.
+//
+
+#ifndef PR1_MODELO_H
+#define PR1_MODELO_H
+
+
+
+namespace PAG{
+    /**
+     * Clase que contiene un modelo, con solo un objeto de la clase Malla
+     * Se comunica directamente con la clase Renderer
+     * Usa al bonito Assimp para cargar los archivos .obj
+     */
+    class Modelo {
+
+    };
+}
+
+
+
+#endif //PR1_MODELO_H
